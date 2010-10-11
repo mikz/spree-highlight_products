c7c5f9d8c9aab489c6f34ebf5b774387df191250
Michal Cichra <michal.cichra@gmail.com>
2010-10-11 17:00:26 +0200

    initial work, extension now boots with spree 0.30

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-map.namespace :admin do |admin|
-  admin.resources :products, :member => {:highlight => :post, :unhighlight => :post}
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :products do
+      member do
+        post :highlight, :unhighlight
+      end
+    end
+  end
 end
